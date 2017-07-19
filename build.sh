b1cb149ce58403ff2a8a03d803c4d631a4384f2e
Sergei Miami <miami@blackcrystal.net>
2017-07-19 13:15:57 +0300

    Added more tags

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-docker build -t miami/phpunit:6.2 . \
-	&& docker push miami/phpunit:6.2
+docker build -t miami/phpunit .
+docker build -t miami/phpunit:latest .
+docker build -t miami/phpunit:6.2 .
+docker push miami/phpunit
+docker push miami/phpunit:latest
+docker push miami/phpunit:6.2
 
